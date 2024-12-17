de1428e3d3c25cfa573ab55824e4442287dd00b4
Devang Jain <devangj870@gmail.com>
2024-12-17 21:42:15 +0530

    added marquee component to display quotes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,14 +1,30 @@
 import React,{useState} from "react";
 import imgUrl from '../assets/images/forrest-gump.png'
+
+import CustomMarquee from "./customMarquee";
 export default function Home(){
+    const [movie, setMovie] = useState()
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(movie)
+    }
+    function handleChange(e){
+        setMovie(e.target.value)
+    }
     return (
         <div className="home-page">
-            <header class="header-banner">
-                <div class="overlay">
+            <header className="header-banner">
+                <div className="overlay">
                     <h1 className="heading">Film Quest</h1>
                 </div>
             </header>
-            <Button variant="contained">Contained</Button>
+            <div className="form-div">
+                <form action="" onSubmit={handleSubmit}>
+                    <input onChange={handleChange} type="text" className="search-field" placeholder="Search for a movie..."/>
+                    <button type="submit" className="search-btn">Search</button>
+                </form>
+            </div>
+            <CustomMarquee/>
         </div>
     )
 }
