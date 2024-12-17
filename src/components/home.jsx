fdcc4f6c480a23d1a74a0a97a67fa867290cf71a
Devang Jain <devangj870@gmail.com>
2024-12-17 20:51:36 +0530

    initialised repo and developed header

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+import imgUrl from '../assets/images/forrest-gump.png'
+export default function Home(){
+    return (
+        <div className="home-page">
+            <header class="header-banner">
+                <div class="overlay">
+                    <h1 className="heading">Film Quest</h1>
+                </div>
+            </header>
+            <Button variant="contained">Contained</Button>
+        </div>
+    )
+}
